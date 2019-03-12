2004c0f9cd10230dbe2fd2dfc24793c68c1cdef2
zhangyonglele <zhangyonglele@yeah.net>
2019-03-12 22:51:08 +0800

    project created

---
+++ src/test/java/com/volunteer/volunteer/VolunteerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.volunteer.volunteer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VolunteerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
