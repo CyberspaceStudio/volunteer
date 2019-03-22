6b10f1d216ec8663d81fee8faab8b996335ac76c
zhangyonglele <zhangyonglele@yeah.net>
2019-03-22 15:54:01 +0800

    添加新增部门功能

--- src/main/java/com/volunteer/volunteer/controller/DepartmentController.java
+++ src/main/java/com/volunteer/volunteer/controller/DepartmentController.java
@@ -2,7 +2,9 @@ package com.volunteer.volunteer.controller;
 
 import com.volunteer.volunteer.model.Department;
 import com.volunteer.volunteer.service.DepartmentService;
+import com.volunteer.volunteer.util.ToolSupport.UniversalResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -14,8 +16,17 @@ public class DepartmentController {
     DepartmentService departmentService;
 
     @GetMapping("/organization/info")
-    List<Department> getDepartmentByOrganization(String organization){
+    public List<Department> getDepartmentByOrganization(String organization){
         return departmentService.findDepartmentByOrganization(organization);
     }
 
+
+    @PostMapping("/department/add")
+    public UniversalResponseBody addNewDepartmentForAOrganization(Department department){
+        if (departmentService.insertNewDepartment(department)){
+            return new UniversalResponseBody(0,"成功");
+        }else{
+            return new UniversalResponseBody(1,"失败");
+        }
+    }
 }
