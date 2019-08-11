f29dc964ac3b3cb079e1fb982871d58e2e5d5fc5
chapaofan <zzmaolin>
2019-08-11 11:34:57 +0800

    对接调试完成，等待微信小程序上线

--- src/main/java/com/volunteer/volunteer/dto/WxMssDto.java
+++ src/main/java/com/volunteer/volunteer/dto/WxMssDto.java
@@ -12,7 +12,7 @@ import java.util.Map;
 public class WxMssDto {
     private String touser;//用户openid
     private String template_id;//模版id
-    private String page = "index";//默认跳到小程序首页
+    private String page = "";//默认跳到小程序首页
     private String form_id;//收集到的用户formid
     private String emphasis_keyword = "";//放大那个推送字段
     private Map<String, TemplateData> data;//推送文字
